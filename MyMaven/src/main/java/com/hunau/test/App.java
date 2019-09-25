4e4a51050bc26cd50bebd7244ee26143124f210c
jlwhy <1556712505@qq.com>
2019-09-25 14:53:42 +0800

    Maven程序实例

---
+++ MyMaven/src/main/java/com/hunau/test/App.java
@@ -0,0 +1,24 @@
+package com.hunau.test;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @param $params$
+ * @Description:
+ * @Return: $returns$
+ * @Author: 焦文垚
+ * @单位：湖南农业大学物联网工程专业
+ * @Date: $date$ $time$
+ * @开发版本：综合练习V0.1
+ */
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
